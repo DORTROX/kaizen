5ff754f22cc170a4c6a670ff11a9ef31ab48c5df
DORTROX プロ <akshat.me2005@gmail.com>
2025-05-08 05:17:46 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,65 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen w-full bg-[url('/GOD.png')] bg-cover bg-center bg-no-repeat">
+        <div className="grid grid-cols-3 gap-4 h-screen">
+          <div className="col-span-1">
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+            <h1 className="coral-pixels text-7xl">DORTROX</h1>
+          </div>
+          <div className="col-span-1"></div>
+          <div className="col-span-1">
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+            <h1 className="coral-pixels text-5xl neon-text">
+              Soon I'll be updating it with something better
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
