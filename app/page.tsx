2bf7a090a89c7c733be4018af4755aa839df8d9a
DORTROX プロ <akshat.me2005@gmail.com>
2025-05-08 05:33:17 +0530

    Fix: resolve ESLint warnings and errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-full bg-[url('/GOD.png')] bg-cover bg-center bg-no-repeat">
+      <div className="h-screen w-full bg-[url(&apos;/GOD.png&apos;)] bg-cover bg-center bg-no-repeat">
         <div className="grid grid-cols-3 gap-4 h-screen">
           <div className="col-span-1">
             <h1 className="coral-pixels text-7xl">DORTROX</h1>
@@ -34,28 +26,28 @@ export default function Home() {
           <div className="col-span-1"></div>
           <div className="col-span-1">
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
             <h1 className="coral-pixels text-5xl neon-text">
-              Soon I'll be updating it with something better
+              Soon I&apos;ll be updating it with something better
             </h1>
           </div>
         </div>
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -7,8 +7,6 @@ import { useTheme } from "next-themes";
 import { useIsSSR } from "@react-aria/ssr";
 import clsx from "clsx";
 
-import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
-
 export interface ThemeSwitchProps {
   className?: string;
   classNames?: SwitchProps["classNames"];
@@ -70,10 +68,12 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
           ),
         })}
       >
+        {/* Todo: Add icons */}
         {!isSelected || isSSR ? (
-          <SunFilledIcon size={22} />
+          <div></div>
         ) : (
-          <MoonFilledIcon size={22} />
+
+          <div></div>
         )}
       </div>
     </Component>
